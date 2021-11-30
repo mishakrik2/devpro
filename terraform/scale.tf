cdf537ee8483b89bbf9309b31337dada54687de0
mishakrik2 <mykhailo.krykunenko@m-krykunenko-2.do.local>
2021-11-30 15:02:11 +0200

    keys are now ignored

--- terraform/scale.tf
+++ terraform/scale.tf
@@ -42,7 +42,6 @@ resource "aws_launch_configuration" "ec2_launch_config" {
  # user_data                   = "${file("./files/front-bootstrap.sh")}"
   instance_type               = var.instance-type
  # associate_public_ip_address = true
-  publoc_dns = true
   key_name                    = var.key-name
 }
 
