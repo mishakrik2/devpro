5e0376768668fce157fbcf67697cea91ee974421
mishakrik2 <mykhailo.krykunenko@m-krykunenko-2.do.local>
2021-12-12 19:09:04 +0200

    removed bastion public ip from outputs

--- terraform/blue/outputs.tf
+++ terraform/blue/outputs.tf
@@ -13,12 +13,6 @@ output "back_instance_public_ip_blue" {
       value       = aws_instance.back_instance_blue.public_ip
 }
 
-
-output "bastion_instance_public_ip_blue" {
-      description = "Public IP address of the EC2 instance blue"  
-      value       = aws_instance.bastion_instance_blue.public_ip
-}
-
 output "alb_dns_name_blue" {
       description = "DNS name of load balancer blue"
       value       = aws_alb.ec2-alb-blue.dns_name
@@ -31,12 +25,6 @@ output "alb_dns_name_blue" {
 output "back_instance_public_ip_green" {
       description = "Public IP address of the EC2 instance green"  
       value       = aws_instance.back_instance_green.public_ip
-}
-
-
-output "bastion_instance_public_ip_green" {
-      description = "Public IP address of the EC2 instance green"  
-      value       = aws_instance.bastion_instance_green.public_ip
 }
 
 output "alb_dns_name_green" {
