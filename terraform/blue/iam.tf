33e7ecdf81dc12227a52b72580c6329983618601
mishakrik2 <mykhailo.krykunenko@m-krykunenko-2.do.local>
2021-12-23 04:41:04 +0200

    added ssm role attachment

--- terraform/blue/iam.tf
+++ terraform/blue/iam.tf
@@ -74,12 +74,12 @@ EOT
 
 resource "aws_iam_policy_attachment" "ssm_attach_core" {
   name       = "test-attachment"
-  role      = "${aws_iam_role.ec2-role.name}"
+  roles     = [aws_iam_role.ec2-role.name]
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
 resource "aws_iam_policy_attachment" "ssm_attach_role" {
   name       = "test-attachment"
-  role      = "${aws_iam_role.ec2-role.name}"
+  roles      = [aws_iam_role.ec2-role.name]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
